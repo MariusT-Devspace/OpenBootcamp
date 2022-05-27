342b960064c3a7e7e81246d02f2a940a2a24242f
MultiP-Dev <mariustichieru@gmail.com>
2022-05-27 10:15:56 +0200

    Subir ejercicio 1 tema 7

---
+++ Python/Tema 7/Ejercicio 1/main.py
@@ -0,0 +1,14 @@
+import operaciones as op
+
+a = 8
+b = 4
+
+suma = op.suma(a, b)
+resta = op.resta(a, b)
+multiplicacion = op.multiplica(a, b)
+division = op.divide(a, b)
+
+print(f"El resultado de sumar {a} mas {b} es: {suma}")
+print(f"El resultado de restar {a} menos {b} es: {resta}")
+print(f"El resultado de multiplicar {a} por {b} es: {multiplicacion}")
+print(f"El resultado de dividir {a} entre {b} es: {division}")
