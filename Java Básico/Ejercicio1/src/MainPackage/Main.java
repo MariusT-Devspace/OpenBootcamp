8cfba9e7882e90116ee6631cdae54015f56cee0b
MultiP-Dev <mariustichieru@gmail.com>
2022-06-09 18:10:13 +0200

    Java básico ejercicio 1

---
+++ Java Básico/Ejercicio1/src/MainPackage/Main.java
@@ -0,0 +1,48 @@
+package MainPackage;
+
+public class Main {
+
+    public static void main(String[] args){
+        // Numéricos
+        // Enteros
+        byte byteType = 64;
+        short shortType = 25346;
+        int integerType = 5326534;
+        long longType = 345233272L;
+        // Decimales
+        float floatType = 43.55f;
+        double doubleType =543.643545d;
+
+        // Texto
+        String stringType = "Soy un String";
+
+        // Boolean
+        boolean boolType = true;
+
+        System.out.println("Tipos de datos");
+        System.out.println("");
+
+        System.out.println("Numéricos");
+        System.out.println("");
+
+        System.out.println("Enteros:");
+        System.out.println("byte: " + byteType);
+        System.out.println("short: " + shortType);
+        System.out.println("int: " + integerType);
+        System.out.println("long: " + longType);
+        System.out.println("");
+
+        System.out.println("Decimales:");
+        System.out.println("float: " + floatType);
+        System.out.println("double: " + doubleType);
+        System.out.println("");
+
+        System.out.println("Texto");
+        System.out.println("String: " + stringType);
+        System.out.println("");
+
+        System.out.println("Boolean");
+        System.out.println("String: " + boolType);
+
+    }
+}
