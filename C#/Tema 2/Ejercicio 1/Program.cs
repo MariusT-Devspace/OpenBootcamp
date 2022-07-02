4ab66e979a2c25efabacd52dae5613e46f5717ba
MultiP-Dev <mariustichieru@gmail.com>
2022-07-02 22:22:09 +0200

    Tema 2 - Ejercicio 1

---
+++ C#/Tema 2/Ejercicio 1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Introduce tus datos:");
+Console.WriteLine();
+
+Console.WriteLine("Nombre:");
+string nombre = Console.ReadLine();
+
+
+Console.WriteLine("Edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Email:");
+string email = Console.ReadLine();
+
+Console.WriteLine("Número de teléfono");
+string phone = Console.ReadLine();
+
+string mensaje = ("Gracias por inscribirte, " + nombre + "!");
+Console.WriteLine();
+Console.WriteLine(mensaje);
