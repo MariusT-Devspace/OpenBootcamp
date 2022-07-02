9dfcc6e05a2577c5f4395cd4fa063cc0e6e894ac
MultiP-Dev <mariustichieru@gmail.com>
2022-07-03 00:02:03 +0200

    Tema 2 - Ejercicio 2

---
+++ C#/Tema 2/Ejercicio 2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Coche:
+int puertas_coche = 3;
+int ruedas_coche = 4;
+string marca_coche = "Ford";
+bool ITV_vigente = true;
+
+Console.WriteLine("Coche:");
+Console.WriteLine();
+Console.WriteLine($"Marca: {marca_coche}");
+Console.WriteLine($"Puertas: {puertas_coche}");
+Console.WriteLine($"Ruedas: {ruedas_coche}");
+Console.WriteLine($"ITV vigente: {ITV_vigente}");
+
+// Mesa:
+int peso_mesa_kg = 100;
+int largo_mesa_cm = 230;
+string material_mesa = "Madera";
+string color_mesa = "Marrón";
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Mesa:");
+Console.WriteLine();
+Console.WriteLine($"Peso: {peso_mesa_kg} kg");
+Console.WriteLine($"Largo: {largo_mesa_cm} cm");
+Console.WriteLine($"Material: {material_mesa}");
+Console.WriteLine($"Color: {color_mesa}");
