6f4fa850586dd7e257e21886fcaa27a056ba1a14
MultiP-Dev <mariustichieru@gmail.com>
2022-07-03 15:41:35 +0200

    Tema 4 - Ejercicio 1

---
+++ C#/Tema 4/Ejercicio 1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Escribe un número:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num} x {i} = {num * i}");
+}
