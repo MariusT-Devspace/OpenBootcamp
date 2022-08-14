620c8fec6a8b6fdbd56a72f0f1b6b6d08203669c
MultiP-Dev <mariustichieru@gmail.com>
2022-08-14 20:45:58 +0200

    Ejercicio Tema 2

---
+++ APIs con .NET/Tema 2/University/UniversityApiBackend/Migrations/20220814184231_Create Courses table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityApiBackend.Migrations
+{
+    public partial class CreateCoursestable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Goal",
+                table: "Courses",
+                newName: "Goals");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Goals",
+                table: "Courses",
+                newName: "Goal");
+        }
+    }
+}
---
+++ APIs con .NET/Tema 2/University/UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string? ShortDescription { get; set; }
+
+        public string? LongDescription { get; set; }
+
+        public string? TargetAudience { get; set; }
+
+        public string? Goals { get; set; }
+
+        public string? Requirements { get; set; }
+
+        public enum Level
+        {
+            Básico = 1,
+            Intermedio,
+            Avanzado
+        }
+
+    }
+}
