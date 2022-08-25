9eab215c2682217aee101c84e52210c5f7cae73f
Marius <mariustichieru@gmail.com>
2022-08-25 12:59:25 +0200

    Lesson 2 Exercise

---
+++ JavaScript Básico/Tema 2/index.js
@@ -0,0 +1,13 @@
+const name = "Pepito";
+const age = 35;
+const isADeveloper = true;
+const birthDate = new Date(1987, 05, 14);
+const favoriteBook = {
+    title : "Structure and Interpretation of Computer Programs JavaScript Edition",
+    author: "Harold Abelson and Gerald Jay Sussman. Adapted to JavaScript by Martin Henz and Tobias Wrigstadwith Julie Sussman",
+    date: 2022,
+    url: "https://books.google.es/books/about/Structure_and_Interpretation_of_Computer.html?id=ah9zzgEACAAJ&redir_esc=y"
+};
+const studentData = [name, age, isADeveloper, birthDate, favoriteBook];
+
+console.log(studentData);
