0c70f49e90b31aec24c3fb84643d5ff7242c5f07
Marius <mariustichieru@gmail.com>
2022-08-31 14:36:16 +0200

    Lesson 13 Exercise

---
+++ JavaScript Básico/Tema 13/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"quotes": [
+			"error",
+			"double"
+		]
+	}
+};
