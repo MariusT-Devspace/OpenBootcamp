c5d2dec17b86b0cac9320fbf55e8b9300e43f271
MultiP-Dev <mariustichieru@gmail.com>
2022-09-23 14:00:53 +0200

    Lesson 6 Exercise

---
+++ APIs con .NET/Tema 6/University/UniversityApiBackend/Helpers/JwtHelpers.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Helpers
+{
+    public static class JwtHelpers
+    {
+        public static IEnumerable<Claim> GetClaims(this UserTokens userAccounts, Guid id)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim("Id", userAccounts.Id.ToString()),
+                new Claim(ClaimTypes.Name, userAccounts.UserName),
+                new Claim(ClaimTypes.Email, userAccounts.EmailId),
+                new Claim(ClaimTypes.NameIdentifier, id.ToString()),
+                new Claim(ClaimTypes.Expiration, DateTime.UtcNow.AddDays(1).ToString("MMM ddd dd yyyy HH:mm:ss tt"))
+            };
+
+            if (userAccounts.UserName == "Admin")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            }
+            else if (userAccounts.UserName == "User 1")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "User"));
+                claims.Add(new Claim("UserOnly", "User 1"));
+            }
+
+            return claims;
+        }
+
+        public static IEnumerable<Claim> GetClaims(this UserTokens userAccounts, out Guid id)
+        {
+            id = Guid.NewGuid();
+            return GetClaims(userAccounts, id);
+        }
+
+        public static UserTokens GenTokenKey(UserTokens model, JwtSettings jwtSettings)
+        {
+            try
+            {
+                var userToken = new UserTokens();
+                if (model == null)
+                {
+                    throw new ArgumentNullException(nameof(model));
+                }
+
+                // Obtain SECRET KEY
+                var key = System.Text.Encoding.ASCII.GetBytes(jwtSettings.IssuerSigningKey);
+
+                Guid id;
+
+                // Expires in 1 Day
+                DateTime expireTime = DateTime.UtcNow.AddDays(1);
+
+                // Validity of our token
+                userToken.Validity = expireTime.TimeOfDay;
+
+                // Generate our JWT
+                var jwToken = new JwtSecurityToken(
+                    issuer: jwtSettings.ValidIssuer,
+                    audience: jwtSettings.ValidAudience,
+                    claims: GetClaims(model, out id),
+                    notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                    expires: new DateTimeOffset(expireTime).DateTime,
+                    signingCredentials: new SigningCredentials(
+                            new SymmetricSecurityKey(key),
+                            SecurityAlgorithms.HmacSha256
+                        )
+                    );
+
+                userToken.Token = new JwtSecurityTokenHandler().WriteToken(jwToken);
+                userToken.UserName = model.UserName;
+                userToken.Id = model.Id;
+                userToken.GuidId = id;
+                return userToken;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error Generating the JWT", e);
+            }
+        }
+    }
+}
---
+++ APIs con .NET/Tema 6/University/UniversityApiBackend/Services/CoursesService.cs
@@ -0,0 +1,22 @@
+﻿using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public class CoursesService : ICoursesService
+    {
+        public IEnumerable<Course> GetCoursesByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Course> GetCoursesWithNoSyllabus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Course> GetStudentCourses(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
